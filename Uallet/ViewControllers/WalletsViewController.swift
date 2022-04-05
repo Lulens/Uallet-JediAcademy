a66dc97f5e9a422cfeea9beeb99fafe7e56b2887
Lulens <lucia.baez@uala.com.ar>
2022-04-05 16:16:03 -0300

    comió de prueba

--- Uallet/ViewControllers/WalletsViewController.swift
+++ Uallet/ViewControllers/WalletsViewController.swift
@@ -104,14 +104,6 @@ class WalletsViewController: UITableViewController {
     }
     */
 
-    /*
-    // MARK: - Navigation
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
     }
 }
